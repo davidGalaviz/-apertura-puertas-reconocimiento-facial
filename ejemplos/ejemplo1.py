289581ad7986fe7c47015f96e9b8e9d7bce304b0
davidGalaviz <david@galaviz.com.mx>
2022-09-23 11:08:17 -0500

    programa y foto

---
+++ ejemplos/ejemplo1.py
@@ -0,0 +1,7 @@
+from deepface import DeepFace
+
+obj = DeepFace.analyze(img_path = "/home/david/Documentos/Git/-apertura-puertas-reconocimiento-facial/deepFace/face1.jpg",actions = ['age', 'gender', 'race', 'emotion'])
+
+
+print("El resultado de la imagen es: ")
+print(obj)
